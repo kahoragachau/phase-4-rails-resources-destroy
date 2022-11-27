ed555c042b89014b180107ebe9dc467d520e2603
kahoragachau <joelkahoragachau6@gmail.com>
2022-11-27 16:24:09 +0300

    - done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :birds, only: [:index, :show, :create, :update, :destroy]
+  # wecan use resources :birds now because we have all 5 routes methods
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
